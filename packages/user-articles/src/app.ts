ebff14c8422e0d484e27f3e7a1d546f06ad044fb
Chin Tiong <alvin.tanct@gmail.com>
2024-03-21 14:59:39 +0000

    added unit and api tests in user-articles poc

---
+++ packages/user-articles/src/app.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+import express from 'express';
+import articleRouter from './routes/articleRoute';
+import authRouter from './routes/authRoute';
+import userRouter from './routes/userRoute';
+
+const app = express();
+app.use(express.json());
+
+// Your code starts here.
+// Placeholders for all requests are provided for your convenience.
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/articles', articleRouter);
+
+export default app;
